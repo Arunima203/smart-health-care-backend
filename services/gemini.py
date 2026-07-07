589f24b0a0d2f434924f17745055b00df11923b8
arunima <senarunima601@gmail.com>
2026-07-07 22:21:41 +0530

    initial backend commit

--- services/gemini.py
+++ services/gemini.py
@@ -1,14 +1,15 @@
-from google import genai
+import os
+from dotenv import load_dotenv
+import google.generativeai as genai
 
-client = genai.GenerativeModel()
+load_dotenv()
 
-genai.configure(api_key=os.getenv("AQ.Ab8RN6Ix8-udDdiNRo8WZ31mUDw9MfZniUyK7M04cy0UrP26Qg"))
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
-print("Gemini client created successfully")
-def get_health_advice(symptoms,language):
-    response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents=f"""
+model = genai.GenerativeModel("gemini-2.5-flash")
+
+def get_health_advice(symptoms, language):
+    response = model.generate_content(f"""
 You are an expert AI doctor.
 
 Reply ONLY in {language} language.
@@ -17,11 +18,10 @@ Patient symptoms:
 {symptoms}
 
 Give:
-
 1. Possible Disease
 2. First Aid
 3. Medicine Suggestion
 4. Emergency Level
-"""
-)
-     return response.text
+""")
+
+    return response.text
